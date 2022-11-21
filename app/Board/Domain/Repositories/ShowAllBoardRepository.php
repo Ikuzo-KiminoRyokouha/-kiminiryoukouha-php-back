d33321b497d0ffa0ed08ad09038d0d8d2084e85a
jinBaeng <jinbaeng88@gmail.com>
2022-11-21 16:19:50 +0900

    update

--- app/Board/Domain/Repositories/ShowAllBoardRepository.php
+++ app/Board/Domain/Repositories/ShowAllBoardRepository.php
@@ -6,7 +6,9 @@ use App\Board\Domain\Entities\Board;
 
 class ShowAllBoardRepository implements ShowAllBoardRepositoryInterface{
     public function show($page):array{
-        $board = Board::skip(($page - 1) * 6)->take(6)
+                $board = Board::skip(($page - 1) * 6)->take(6)                    ->with(['user'=> function ($query) {
+                    $query->select(['name','id']);
+                }])
                 ->get(['id' , 'title' , 'content','user_id','created_at','updated_at','private']);
         $pages = ceil(Board::count()/6);
         return [
