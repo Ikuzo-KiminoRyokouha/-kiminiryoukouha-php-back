86d205eb686009a0bd35a2cbb0a2a793c6170fb4
jinBaeng <jinbaeng88@gmail.com>
2022-11-21 10:39:58 +0900

    update migration private

--- app/Board/Domain/Repositories/ShowAllBoardRepository.php
+++ app/Board/Domain/Repositories/ShowAllBoardRepository.php
@@ -8,7 +8,7 @@ class ShowAllBoardRepository implements ShowAllBoardRepositoryInterface{
     public function show($page):array{
         $board = Board::skip(($page - 1) * 6)->take(6)
                 ->get(['id' , 'title' , 'content','user_id','created_at','updated_at','private']);
-        $pages = ceil(Board::count()/10);
+        $pages = ceil(Board::count()/6);
         return [
             'pages' => $pages,
             'boards' => $board
